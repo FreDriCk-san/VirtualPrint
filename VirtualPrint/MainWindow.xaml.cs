ae3b29c9b12e4ba2ffb259118ccaabde02409e34
FreDriCk-san <ferdinonand@gmail.com>
2019-02-24 16:58:42 +0700

    Check position for print

--- VirtualPrint/MainWindow.xaml.cs
+++ VirtualPrint/MainWindow.xaml.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Documents;
 using System.Windows.Media.Imaging;
 
 namespace VirtualPrint
@@ -35,20 +34,63 @@ namespace VirtualPrint
         {
             var printDLG = new PrintDialog();
 
+            var arrayOfModels = new Model.TextModel[]
+            {
+                new Model.TextModel(numTextBox.Text, 590, 120),
+                new Model.TextModel(day.Text, 195, 140),
+                new Model.TextModel(month.Text, 280, 140),
+                new Model.TextModel(year.Text, 380, 140),
+                new Model.TextModel(carBrand.Text, 240, 240),
+                new Model.TextModel(licensePlate.Text, 330, 260),
+                new Model.TextModel(driver.Text, 180, 280),
+                new Model.TextModel(certificate.Text, 230, 310),
+                new Model.TextModel(persNum.Text, 890, 275),
+                new Model.TextModel(departure.Text, 190, 513),
+                new Model.TextModel(returns.Text, 190, 535),
+                new Model.TextModel(orderBySome.Text, 150, 650),
+                //new Model.TextModel(workType.Text, 0, 0),
+                //new Model.TextModel(dispatcherSign.Text, 0, 0),
+                //new Model.TextModel(mechanicSign.Text, 0, 0),
+                //new Model.TextModel(driverSign.Text, 0, 0),
+                //new Model.TextModel(passedDriver.Text, 0, 0),
+                //new Model.TextModel(acceptedMech.Text, 0, 0)
+            };
+
+            var arrayOfTextBlocks = new TextBlock[arrayOfModels.Length];
+
             if (printDLG.ShowDialog() == true)
             {
-                var text = new Run("ЗАРАБОТАЛО!!!!");
+                try
+                {
+                    var index = 0;
 
-                var textBlock = new TextBlock();
-                textBlock.Inlines.Add(numTextBox.Text);
-                textBlock.Margin = new Thickness(50, 20, 0, 0);
+                    foreach (var item in arrayOfModels)
+                    {
+                        var textBlock = new TextBlock();
+                        textBlock.Inlines.Add(item.Content);
+                        textBlock.Margin = new Thickness(item.Xposition, item.YPosition, 0, 0);
 
-                var pageSize = new Size(printDLG.PrintableAreaWidth, printDLG.PrintableAreaHeight);
-                textBlock.Measure(pageSize);
-                textBlock.Arrange(new Rect(370, 60, pageSize.Width, pageSize.Height));
+                        var pageSize = new Size(printDLG.PrintableAreaWidth, printDLG.PrintableAreaHeight);
+                        textBlock.Measure(pageSize);
+                        textBlock.Arrange(new Rect(item.Xposition, item.YPosition, pageSize.Width, pageSize.Height));
+                        arrayOfTextBlocks[index] = textBlock;
 
-                printDLG.PrintVisual(textBlock, "Visual element printing (VS)");
+                        index++;
+                    }
 
+                    var tempGrid = new Grid();
+
+                    foreach (var item in arrayOfTextBlocks)
+                    {
+                        tempGrid.Children.Add(item);
+                    }
+
+                    printDLG.PrintVisual(tempGrid, "Visual element printing (VS)");
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show($"ОШИБКА: {exception.Message}. Свяжитесь с разработчиком!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
             }
         }
     }
---
+++ VirtualPrint/Model/TextModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPrint.Model
+{
+    public class TextModel
+    {
+        private string content;
+        private double xPosition;
+        private double yPosition;
+
+        public string Content
+        {
+            get { return content; }
+        }
+
+        public double Xposition
+        {
+            get { return xPosition; }
+        }
+
+        public double YPosition
+        {
+            get { return yPosition; }
+        }
+
+        public TextModel(string content, double xPosition, double yPosition)
+        {
+            this.content = content;
+            this.xPosition = xPosition;
+            this.yPosition = yPosition;
+        }
+    }
+}
